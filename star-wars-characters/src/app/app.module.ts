2687ab55720cb3aed5656e155e1f3ab0f7cd9d00
Angjel <angjel.spasovski@gmail.com>
2018-12-02 16:51:39 +0100

    Create Character

--- star-wars-characters/src/app/app.module.ts
+++ star-wars-characters/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { ListComponent } from './components/list/list.component';
 import { ItemComponent } from './components/item/item.component';
 import { StarWarsService } from './star-wars.service';
 import { LogService } from './log.service';
+import { CreateCharacterComponent } from './components/create-character/create-character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabsComponent,
     ListComponent,
-    ItemComponent
+    ItemComponent,
+    CreateCharacterComponent
   ],
   imports: [
     BrowserModule,
