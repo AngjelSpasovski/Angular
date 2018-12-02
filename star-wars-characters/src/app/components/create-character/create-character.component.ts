e28499ada59e045eca292b116236a3f3771f524a
Angjel <angjel.spasovski@gmail.com>
2018-12-02 16:49:08 +0100

    Create Character component and form

---
+++ star-wars-characters/src/app/components/create-character/create-character.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-character',
+  templateUrl: './create-character.component.html',
+  styleUrls: ['./create-character.component.css']
+})
+export class CreateCharacterComponent implements OnInit {
+
+  availableSides = [
+    { display: "None",    value: ""},
+    { display: "Light",   value: "light"},
+    { display: "Dark",    value: "dark"}
+
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
