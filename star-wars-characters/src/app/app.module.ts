8e1ecf36d8e1778fc4d99cdb21321a62bda141f6
Angjel <angjel.spasovski@gmail.com>
2018-12-12 23:38:58 +0100

    Setting up the Routes

--- star-wars-characters/src/app/app.module.ts
+++ star-wars-characters/src/app/app.module.ts
@@ -1,15 +1,26 @@
+// Angular core
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// Two Way Data Binding module
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // Two Way Data Binding module
+import { RouterModule, Route } from '@angular/router' // Router
 
+// Components
 import { AppComponent } from './app.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { ListComponent } from './components/list/list.component';
 import { ItemComponent } from './components/item/item.component';
+import { CreateCharacterComponent } from './components/create-character/create-character.component';
+import { HeaderComponent } from './components/header/header.component';
+
+// Services
 import { StarWarsService } from './star-wars.service';
 import { LogService } from './log.service';
-import { CreateCharacterComponent } from './components/create-character/create-character.component';
+
+// Routes are Array of JavaScript Objects
+const routes = [
+  { path:'', component: TabsComponent },
+  { path:'new-character', component: CreateCharacterComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,11 +28,13 @@ import { CreateCharacterComponent } from './components/create-character/create-c
     TabsComponent,
     ListComponent,
     ItemComponent,
-    CreateCharacterComponent
+    CreateCharacterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
